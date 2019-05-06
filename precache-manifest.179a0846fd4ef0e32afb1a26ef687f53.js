9735dffc3d6f4725f65ef9351982f988434a80d4
Cezar Junior de Souza <cezar08@unochapeco.edu.br>
2019-05-05 22:31:16 -0300

    first

---
+++ precache-manifest.179a0846fd4ef0e32afb1a26ef687f53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9f1993415fca0c6287bda397fa1427f",
+    "url": "/hacking-health-front/static/media/logo.a9f19934.png"
+  },
+  {
+    "revision": "aab6406a027c16c1bf5595d0956227f4",
+    "url": "/hacking-health-front/static/media/Crianca.aab6406a.png"
+  },
+  {
+    "revision": "446c4c3f9dad5b4cdf43",
+    "url": "/hacking-health-front/static/js/runtime~main.446c4c3f.js"
+  },
+  {
+    "revision": "2083fe19a38b38f7ba1c",
+    "url": "/hacking-health-front/static/js/main.2083fe19.chunk.js"
+  },
+  {
+    "revision": "7733527c43ee6212d336",
+    "url": "/hacking-health-front/static/js/2.7733527c.chunk.js"
+  },
+  {
+    "revision": "2083fe19a38b38f7ba1c",
+    "url": "/hacking-health-front/static/css/main.66d19093.chunk.css"
+  },
+  {
+    "revision": "6b06f4cbeca760aa9f00a54c4a2fca7b",
+    "url": "/hacking-health-front/index.html"
+  }
+];
